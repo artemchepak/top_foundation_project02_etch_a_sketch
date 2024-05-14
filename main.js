94908afe592dc9d5dbe8077909ab14f84e92e0ea
Artem Chepak <a.chepak91@gmail.com>
2024-05-14 11:59:39 -0600

    add hover function

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ const input = document.createElement('input');
 const btn = document.createElement('button');
 const gameBox = document.createElement('div');
 //add more advanced controlls after implementing basic functionality
-// const controls = document.createElement('div'); 
+// const controls = document.createElement('div');
+let gridSize = 16;
 
 title.classList.add('title');
 btn.classList.add('btn');
@@ -15,7 +16,7 @@ input.classList.add('input');
 
 title.innerHTML = 'Etch-a-Sketch';
 btn.innerHTML = 'Button';
-input.placeholder = 'Enter grid size'
+input.placeholder = 'Enter grid row size'
 
 inputBox.appendChild(input);
 inputBox.appendChild(btn);
@@ -23,3 +24,47 @@ inputBox.appendChild(btn);
 container.appendChild(title);
 container.appendChild(inputBox);
 container.appendChild(gameBox);
+
+//function allows user to enter only numbers
+input.onkeydown = function (event) {
+    if (isNaN(event.key) && event.key !== 'Backspace') {
+        event.preventDefault();
+    }
+};
+
+btn.addEventListener('click', () => {
+    //add size limit
+    gridSize = parseInt(input.value);
+    createGrid(gridSize);
+    addHover();
+});
+
+function createGrid(size) {
+    i = 0;
+    while (i < (size * size)) {
+        let gameItem = document.createElement('div');
+        gameItem.innerHTML = '';
+        gameItem.classList.add('gameItem');
+        gameItem.style.width = 100 / size + '%';
+
+        gameBox.appendChild(gameItem);
+        i++;
+    }
+}
+
+function addHover() {
+    let gameItems = document.querySelectorAll('.gameItem');
+
+    Array.from(gameItems).forEach(function (gameItem) {
+        gameItem.addEventListener('mouseenter', (event) => {
+            event.target.classList.add('dark-bgc');
+        });
+    });
+}
+
+function startGame() {
+    createGrid(gridSize);
+    addHover();
+}
+
+startGame();
